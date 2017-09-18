4bd09cfeb6625fa59629254d44cc8419065bc822
Joel Clarke <joel.clarke273@gmail.com>
2017-09-19 00:24:05 +0100

    Add user existence check for Login page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home/Home'
 import Login from '@/components/Login/Login'
+import Store from '@/store'
 
 Vue.use(Router)
 
@@ -17,7 +18,14 @@ export default new Router({
     {
       path: '/',
       name: 'Login',
-      component: Login
+      component: Login,
+      beforeEnter: (to, from, next) => {
+        console.log(Store)
+        if (Store.state.auth.user) {
+          next({path: '/home'})
+        }
+        next()
+      }
     }
   ]
 })
