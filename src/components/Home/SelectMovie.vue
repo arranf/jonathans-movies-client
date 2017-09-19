696d75eb12daff5504b562698e1b59c0cf2c71b2
Arran France <arranf@gmail.com>
2017-09-19 13:57:14 +0100

    Add SelectMovie component

---
+++ src/components/Home/SelectMovie.vue
@@ -0,0 +1,28 @@
+<template>
+  <swiper :options="swiperOption" :not-next-tick="notNextTick" ref="mySwiper">
+        <swiper-slide v-for="option in options" @click="vote(option._id)">
+            {{option.Name}}
+        </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+require('swiper/dist/css/swiper.css')
+
+export default {
+  name: 'SelectMovie',
+  props: ['options'],
+  data() {
+      return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                slidesPerView: 3,
+                paginationClickable: true,
+                spaceBetween: 30,
+                mousewheelControl : true
+            }
+      }
+  }
+}
+</script>
