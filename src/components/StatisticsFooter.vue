6ced01469693bea364d2615b565c92890fd26315
Arran France <arranf@gmail.com>
2017-09-19 12:11:53 +0100

    Semi Working Statistics Footer

--- src/components/StatisticsFooter.vue
+++ src/components/StatisticsFooter.vue
@@ -6,19 +6,19 @@
                     <div class="card-deck">
                         <div class="card">
                             <div class="card-body">
-                                <p class="card-text" v-if="getPolls"><strong>{{getPolls.length}}</strong> Polls Made</p>
+                                <p class="card-text" v-if="polls"><strong>{{polls.length}}</strong> Polls Made</p>
                             </div>
                         </div>
 
                         <div class="card">
                             <div class="card-body">
-                                <p class="card-text" v-if="getVotes"><strong>{{getVotes.length}}</strong> Votes Cast</p>
+                                <p class="card-text" v-if="votes"><strong>{{votes.length}}</strong> Votes Cast</p>
                             </div>
                         </div>
 
                         <div class="card">
                             <div class="card-body">
-                                <p class="card-text"><strong>1</strong> <a href="http://www.imdb.com/title/tt3508840/" class="card-link">The Assassin</a></p>
+                                <p class="card-text"><strong>1</strong> <a target="_blank" href="http://www.imdb.com/title/tt3508840/" class="card-link">The Assassin</a></p>
                             </div>
                         </div>
                     </div>
@@ -28,6 +28,7 @@
 
         <div class="row">
             <div class="col mt-4">
+                <a class="btn btn-primary" @click="test()">Test</a>
                 <p>Made with <i class="fa fa-heart heart" aria-hidden="true"></i> <span class="sr-only">love</span> (and a lot of food from Spar) by Arran and Joel.</p>
             </div>
         </div>
@@ -35,14 +36,30 @@
 </template>
 
 <script>
-import {mapGetters, mapState} from 'vuex'
+import {mapGetters, mapState, mapActions} from 'vuex'
 
 export default {
   name: 'StatisticsFooter',
+  data() {
+      return {
+          gotVoteandPolls: false
+      }
+  },
   computed: {
-      ...mapGetters('vote', {getVotes: 'list'}),
-      ...mapGetters('poll', {getPolls: 'list'}),
+      ...mapGetters('vote', {votes: 'list'}),
+      ...mapGetters('poll', {polls: 'list'}),
       ...mapState('auth', ['user'])
+  },
+  methods: {
+      ...mapActions('vote', {getVotes: 'find'}),
+      ...mapActions('poll', {getPolls: 'find'}),
+  },
+  beforeUpdate: function() {
+      if (this.user && !this.gotVoteandPolls){
+        this.getPolls({query:{}})
+        .then(this.getVotes({query: {}}))
+        .then(this.gotVoteandPolls = true)
+      }
   }
 }
 </script>
