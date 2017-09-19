f8c86ce73a5a4ab41d109bdc63fa64d26d39e25b
Arran France <arranf@gmail.com>
2017-09-19 15:46:40 +0100

    Add rudimentary cover picker

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="container">
-        <div class="row">
+    <div class="container h-80">
+        <div class="d-flex h-100 align-items-stretch">
+            <div class="col" v-if="this.polls">
+                <select-movie :movies="options" :pollId="this.polls[0].id"></select-movie>
+            </div>
             <div class="col">
+
             </div>
         </div>
     </div>
@@ -9,21 +13,36 @@
 
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex'
+import SelectMovie from '@/components/Home/SelectMovie'
 
 export default {
     name:'Home',
+    components: {
+        SelectMovie
+    },
+    data() {
+        return {
+            colors: ['#c62828', ]
+        }
+    },
     computed:{
         ...mapGetters('poll', {
             polls: 'list'
         }),
+        ...mapGetters('option', {
+            options: 'list'
+        })
     },
     methods: {
         ...mapActions('poll', {
             getPoll: 'find'
+        }),
+        ...mapActions('option', {
+            getOptions: 'find'
         })
     },
     created: function () {
-        const query = {
+        const pollQuery = {
             $sort: {endDateTime: 1},
             $limit: 1,
             startDateTime: {
@@ -33,6 +52,14 @@ export default {
                 $lt: new Date().getTime()
             }
         }
+        
+        this.getPoll({pollQuery})
+        .then(this.getOptions({query: {id: this.polls[0].id}}))
+        .catch(error => console.error(error))
     }
 }
 </script>
+
+<style scoped>
+
+</style>
--- src/components/Home/SelectMovie.vue
+++ src/components/Home/SelectMovie.vue
@@ -1,28 +1,75 @@
 <template>
-  <swiper :options="swiperOption" :not-next-tick="notNextTick" ref="mySwiper">
-        <swiper-slide v-for="option in options" @click="vote(option._id)">
-            {{option.Name}}
-        </swiper-slide>
+  <swiper :options="swiperOption" class="swiper-box">
+        <template v-for="movie in movies" >
+            <swiper-slide class="swiper-item" :style="{backgroundColor: randomColor()}" :key="movie._id" @click="vote(movie._id)">{{movie.name}}</swiper-slide>
+        </template>
   </swiper>
 </template>
 
 <script>
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import {mapActions, mapState, mapGetters} from 'vuex'
 require('swiper/dist/css/swiper.css')
 
 export default {
   name: 'SelectMovie',
-  props: ['options'],
+  props: ['movies', 'pollId'],
   data() {
       return {
             swiperOption: {
-                pagination: '.swiper-pagination',
                 slidesPerView: 3,
-                paginationClickable: true,
-                spaceBetween: 30,
-                mousewheelControl : true
-            }
+                spaceBetween: 40,
+                mousewheelControl : true,
+                observeParents:true,
+            },
+            colors: ['#6a1b9a', '#4527a0', '#c62828', '#283593', '#1565c0', '#0277bd', '#00838f', '#00695c', '#2e7d32', '#ff8f00', '#d84315']
+      }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  },
+  methods: {
+      ...mapActions('vote', [{voteAdd: 'create'}, {voteRemove: 'remove'}]),
+      randomColor: function (){
+          return this.colors[Math.floor(Math.random() * this.colors.length)]
+      },
+      vote: function(movieId){
+          if (votes.contains(movieId)){
+              // remove
+          } else {
+              // add
+          }
       }
   }
 }
 </script>
+
+<style scoped>
+.swiper-box {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+  }
+  .swiper-item {
+    height: 100%;
+    text-align: center;
+    font-size: 1.8em ;
+    color: white;
+    /* Center slide text vertically */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    cursor: pointer; 
+    cursor: hand; 
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+  }
+  </style>
