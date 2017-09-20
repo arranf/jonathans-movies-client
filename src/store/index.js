8bf97cc4be0e0d234bee7836304504b9753a4c17
Arran France <arranf@gmail.com>
2017-09-20 13:32:31 +0100

    Add getters to check for active poll and get active poll

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,21 @@ const store = new Vuex.Store({
   plugins: [
     service('vote'),
     service('option'),
-    service('poll'),
+    service('poll', {getters: {
+      getActivePoll (state) {
+        console.log(state)
+        let currentDateTime = new Date().getTime()
+        return Object.values(state.keyedById)
+              .sort((a, b) => a.endDateTime < a.endDateTime ? -1 : 1)
+              .find(p => p.endDateTime > currentDateTime)
+      },
+      isActivePoll (state) {
+        console.log(state)
+        let currentDateTime = new Date().getTime()
+        let polls = Object.values(state.keyedById)
+        return polls.some(p => p.startDateTime <= currentDateTime && p.endDateTime > currentDateTime)
+      }
+    }}),
     service('users'),
     auth({userService: 'users'})
   ]
