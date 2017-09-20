7d8c430f79163e233e0175c77e339a9d62199736
Arran France <arranf@gmail.com>
2017-09-20 23:51:48 +0100

    Move colors into constant

--- src/components/Home/SelectMovie.vue
+++ src/components/Home/SelectMovie.vue
@@ -23,6 +23,8 @@
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
 import {mapActions, mapState, mapGetters} from 'vuex'
 import utils from '@/utils'
+import constants from '@/constants'
+
 require('swiper/dist/css/swiper.css')
 
 export default {
@@ -32,7 +34,7 @@ export default {
       return {
         movieColors: {
             currentIndex: 0,
-            colors: ['#6a1b9a', '#4527a0', '#c62828', '#283593', '#1565c0', '#0277bd', '#00838f', '#00695c', '#2e7d32', '#558B2F', '#9E9D24', '#F9A825', '#4E342E', '#424242', '#ff8f00', '#d84315'],
+            colors: constants.colors['800'],
             movieColorMap: {}
         },
         notNextTick: true,
