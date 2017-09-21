2b3d9948550bfc84de6cb92cd399511399956295
Arran France <arranf@gmail.com>
2017-09-21 10:58:49 +0100

    Add Results component for testing

---
+++ src/components/Results/Results.vue
@@ -0,0 +1,42 @@
+<template>
+   <pie-chart :chart-data="datacollection"></pie-chart>
+</template>
+
+<script>
+import PieChart from './PieChart.js'
+import {mapActions, mapGetters} from 'vuex'
+
+export default {
+  name: 'Results',
+  components: {
+    PieChart
+  },
+  data () {
+    return {
+      datacollection: null
+    }
+  },
+  computed: {
+    ...mapGetters('vote', ['getVotesByOption']),
+    ...mapGetters('poll', ['getMostRecentPoll'])
+  },
+  methods: {
+    ...mapActions('poll', {
+      getPolls: 'find'
+    }),
+    getData: function(){
+      this.getMostRecentPoll
+      // .then(poll => this.getVotesByOption(poll._id))
+      // .then(data => this.data = data)
+      // const labels = getOptionLabel  s()
+    }
+  },
+  created () {
+    this.getPolls()
+  },
+  mounted () {
+    this.getData()
+  }
+}
+</script>
+
