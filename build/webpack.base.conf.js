04445434f6451d2f8cdfaad91cb215eb5fdeb1e7
Arran France <arranf@gmail.com>
2017-09-23 20:00:24 +0100

    Remove unused Bootstrap JS dependencies

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -67,13 +67,5 @@ module.exports = {
         }
       }
     ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default']
-    })
-  ]
+  }
 }
