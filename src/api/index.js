55099235f31eac745072025ffd870b31eae0ccc8
Arran France <arranf@gmail.com>
2017-09-23 15:24:17 +0100

    Add getVotesForMostRecentPoll to queries

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,19 @@ const queries = {
   getCurrentPoll: function () {
     return store.dispatch('poll/find', {query: {
       $sort: {startDateTime: -1},
-      $limit: 100,
+      $limit: 100
       // startDateTime: {
       //   $lt: store.getters['time/getNow']
       // }
       // endDateTime: {
       //   $gte: store.getters['time/getNow']
       // }
+    }})
+  },
+  getVotesForMostRecentPoll: function (pollId) {
+    return store.dispatch('vote/find', {query: {
+      $limit: 100,
+      poll_id: pollId
     }})
   }
 }
