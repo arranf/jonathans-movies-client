dc76b6b61137755fe3f1f965a5ce4123c8764669
Arran France <arranf@gmail.com>
2017-10-02 16:49:06 +0100

    Disable pagination on queries

--- src/api/index.js
+++ src/api/index.js
@@ -14,25 +14,28 @@ const queries = {
     }})
   },
   getVotesForMostRecentPoll: function (pollId) {
-    return store.dispatch('vote/find', {query: {
-      $limit: 100,
-      poll_id: pollId
-    }})
+    return store.dispatch('vote/find', {pageinate: false,
+      query: {
+        $limit: 100,
+        poll_id: pollId
+      }})
   },
   getOptionsForMostRecentPoll: function (pollId) {
-    return store.dispatch('option/find', {query: {
-      $limit: 100,
-      poll_id: pollId
-    }})
+    return store.dispatch('option/find', {pageinate: false,
+      query: {
+        $limit: 100,
+        poll_id: pollId
+      }})
   },
   getVotesForCurrentPoll: function () {
     const poll = store.getters['poll/getActivePoll']
     const pollId = poll._id
     if (poll && pollId) {
-      return store.dispatch('vote/find', {query: {
-        $limit: 1000,
-        poll_id: pollId
-      }})
+      return store.dispatch('vote/find', {pageinate: false,
+        query: {
+          $limit: 1000,
+          poll_id: pollId
+        }})
     }
     return Promise.reject(new Error('Could not get current poll'))
   }
