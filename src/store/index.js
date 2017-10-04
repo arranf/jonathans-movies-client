30e557fe4a1ef8806621d3cc570910c90c1c2b61
Arran France <arranf@gmail.com>
2017-10-04 22:05:31 +0100

    Remove merge delimeters

--- src/store/index.js
+++ src/store/index.js
@@ -10,10 +10,6 @@ import filmsGetters from './films/getters'
 
 import time from '@/store/time'
 import errors from '@/store/errors'
-<<<<<<< HEAD
-import utils from '@/utils'
-=======
->>>>>>> feature-movie-suggest
 
 Vue.use(Vuex)
 
@@ -28,10 +24,6 @@ const store = new Vuex.Store({
     service('vote', {getters: voteGetters}),
     service('option', {getters: optionGetters}),
     service('poll', {getters: pollGetters}),
-<<<<<<< HEAD
-    service('films', {getters: filmsGetters}),
-=======
->>>>>>> feature-movie-suggest
     service('users'),
     service('films', {getters: filmsGetters}),
     auth({userService: 'users'})
