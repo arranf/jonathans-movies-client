d2152cbf382f8c113940592cc9d547e8d3d4b4ca
Arran France <arranf@gmail.com>
2017-10-17 00:53:44 +0100

    Adjust sizes to better accomodate smaller devices

--- src/components/Home/Nominated/OptionPreview.vue
+++ src/components/Home/Nominated/OptionPreview.vue
@@ -1,8 +1,8 @@
 <template>
 <div style="width: 15rem !important;">
-  <div class="mt-2 mb-1 mx-2"  v-if="option" >
+  <div class="mt-2 mb-1 mx-2" style="width: 12rem;"  v-if="option" >
     <img class="img-fluid img-thumbnail" v-if="option.film && option.film.data" v-once :src="getFilmPoster" :alt="option.name + ' image'">
-    <div v-else class="d-flex" style="height: 20.5rem;" :style="{backgroundColor: getColor()}">
+    <div v-else class="d-flex" style="height: 17.5rem;" :style="{backgroundColor: getColor()}">
       <div class="h-30 w-100 align-self-end">
         <p style="font-size: 1.6em;" class="text-white text-center">{{option.name}}</p>
       </div>
