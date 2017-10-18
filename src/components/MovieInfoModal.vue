4567250e7c83f94d12a45834abb9723ec024e73e
Arran France <arranf@gmail.com>
2017-10-18 18:05:05 +0100

    Make movie modal a component

---
+++ src/components/MovieInfoModal.vue
@@ -0,0 +1,84 @@
+<template>
+  <modal :pivotY="0.05" :name="`${film._id}-detail`" height="auto" width="85%" @opened="modalOpened()" :scrollable="true">
+      <div class="card" v-if="film.data">
+        <img class="card-img-top" :src="backdropImage" :alt="`{film.name} Backdrop`">
+        <div class="card-body">
+          <h4 class="card-title">{{film.name}} <small>{{getFilmYear}}</small></h4>
+          <p class="text-muted"><span class="font-weight-bold">Runtime</span>: {{film.data.runtime}} mins | <span class="font-weight-bold">Genres</span> {{film.genres.join(', ')}}</p>
+          <p class="card-text">{{film.overview}}</p>
+        </div>
+        <div class="card-body">
+          <a v-if="showNominate && isCurrentPollInNomination" href="#" @click="addNomination()" :class="shouldNominate" class="card-link font-weight-bold">Nominate</a>
+          <a v-once :href="getImdbLink" target="_blank" class="card-link">More Information</a>
+        </div>
+      </div>
+      <loading-bounce v-else></loading-bounce>
+    </modal>
+</template>
+
+<script>
+import queries from '@/api'
+import utils from '@/utils'
+import tmdbApi from '@/api/tmdb'
+import {mapGetters} from 'vuex'
+import LoadingBounce from '@/components/Loading/LoadingBounce'
+
+export default {
+  name: 'MovieInfoModal',
+  props:  ['film', 'showNominate'],
+    components :{
+      LoadingBounce
+    },
+    methods: {
+      showModal: function() {
+         this.$modal.show(`${this.film._id}-detail`);
+      },
+      hideModal: function () {
+        this.$modal.hide(`${this.film._id}-detail`)
+      },
+      modalOpened: function () {
+        tmdbApi.getMovieData(this.film.tmdb_id)
+          .then(response => this.$set(this.film, 'data', response.data))
+          .catch(error => {console.error(error); this.hideModal()})
+          
+      },
+      addNomination: function () {
+        if (showNominate) {
+          queries.addNomination(this.film)
+            .then(() => {this.hideModal(); this.$router.push('/')})
+            .catch(error => console.error(error))
+        }
+      }
+    },
+    computed: {
+      ...mapGetters('option', ['hasNominationsRemaining', 'hasNominationsRemaining']),
+      ...mapGetters('poll', ['isCurrentPollInNomination']),
+      backdropImage: function () {
+        if (this.film.data) {
+          return utils.getTmdbBackdropImage(this.film.data.backdrop_path)
+        }
+        return ''
+      },
+      getImdbLink: function () {
+        return `https://www.imdb.com/title/${this.film.data.imdb_id}`
+      },
+      getFilmYear: function(){
+        if (this.film && this.film.release_date) {
+          return utils.getYearFromTmdbReleaseDate(this.film.release_date)
+        }
+      },
+      shouldNominate: function () {
+        return {
+          shouldNominate: {'text-muted': !this.hasNominationsRemaining, 'not-active': !this.hasNominationsRemaining}
+        }
+      }
+    }
+}
+</script>
+
+<style scoped>
+.not-active {
+   pointer-events: none;
+   cursor: default;
+}
+</style>
--- src/components/Nominate/FilmSuggestion.vue
+++ src/components/Nominate/FilmSuggestion.vue
@@ -1,23 +1,9 @@
 <template>
   <div>
-    <modal :pivotY="0.05" :name="`${film._id}-detail`" height="auto" width="85%" @opened="modalOpened()" :scrollable="true">
-      <div class="card" v-if="film.data">
-        <img class="card-img-top" :src="backdropImage" :alt="`{film.name} Backdrop`">
-        <div class="card-body">
-          <h4 class="card-title">{{film.name}} <small>{{getFilmYear}}</small></h4>
-          <p class="text-muted"><span class="font-weight-bold">Runtime</span>: {{film.data.runtime}} mins | <span class="font-weight-bold">Genres</span> {{film.genres.join(', ')}}</p>
-          <p class="card-text">{{film.overview}}</p>
-        </div>
-        <div class="card-body">
-          <a v-if="isCurrentPollInNomination" href="#" @click="addNomination()" :class="shouldNominate" class="card-link font-weight-bold">Nominate</a>
-          <a v-once :href="getImdbLink" target="_blank" class="card-link">More Information</a>
-        </div>
-      </div>
-      <loading-bounce v-else></loading-bounce>
-    </modal>
+    <movie-info-modal ref="modal" :film="film" :show-nominate="true"></movie-info-modal>
 
     <div class="card-group">
-      <div class="card" @click="showDetail()">
+      <div class="card" @click="showModal()">
         <div class="card-body">
           <h4 class="card-title d-inline">{{film.name}} <small>{{getFilmYear}}</small></h4> 
         </div>
@@ -42,46 +28,30 @@
 <script>
 import queries from '@/api'
 import utils from '@/utils'
-import tmdbApi from '@/api/tmdb'
 import {mapGetters} from 'vuex'
-import LoadingBounce from '@/components/Loading/LoadingBounce'
+
+import MovieInfoModal from '@/components/MovieInfoModal'
 
   export default {
     props:  ['film'],
-    components :{
-      LoadingBounce
+    components: {
+      MovieInfoModal
     },
     methods: {
-      showDetail: function() {
-         this.$modal.show(`${this.film._id}-detail`);
+      showModal: function() {
+         this.$refs.modal.showModal()
       },
-      hideDetail: function () {
-        this.$modal.hide(`${this.film._id}-detail`)
-      },
-      modalOpened: function () {
-        tmdbApi.getMovieData(this.film.tmdb_id)
-          .then(response => this.$set(this.film, 'data', response.data))
-          .catch(error => {console.error(error); this.hideDetail()})
-          
+      hideModal: function () {
+        this.$refs.modal.hideModal()
       },
       addNomination: function () {
         queries.addNomination(this.film)
-          .then(() => {this.hideDetail(); this.$router.push('/')})
+          .then(() => {this.hideModal(); this.$router.push('/')})
           .catch(error => console.error(error))
       }
     },
     computed: {
-      ...mapGetters('option', ['hasNominationsRemaining', 'hasNominationsRemaining']),
-      ...mapGetters('poll', ['isCurrentPollInNomination']),
-      backdropImage: function () {
-        if (this.film.data) {
-          return utils.getTmdbBackdropImage(this.film.data.backdrop_path)
-        }
-        return ''
-      },
-      getImdbLink: function () {
-        return `https://www.imdb.com/title/${this.film.data.imdb_id}`
-      },
+      ...mapGetters('option', ['hasNominationsRemaining']),
       getFilmYear: function(){
         if (this.film && this.film.release_date) {
           return utils.getYearFromTmdbReleaseDate(this.film.release_date)
@@ -95,10 +65,3 @@ import LoadingBounce from '@/components/Loading/LoadingBounce'
     }
   }
 </script>
-
-<style scoped>
-.not-active {
-   pointer-events: none;
-   cursor: default;
-}
-</style>
