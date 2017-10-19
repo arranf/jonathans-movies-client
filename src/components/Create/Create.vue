831b50635daad7061b2aed5582caf53f5d6b611d
Joel Clarke <joel.clarke273@gmail.com>
2017-10-19 18:13:29 +0100

    Add nomination section to form

--- src/components/Create/Create.vue
+++ src/components/Create/Create.vue
@@ -6,6 +6,7 @@
         <form autocomplete="off">
           <input autocomplete="false" name="hidden" type="text" style="display:none;">
          
+          <!-- Options Input -->
           <div class="form-group">
             <div v-for="(option, index) in options" :key="index">
               <div class="row my-1">
@@ -15,7 +16,7 @@
                 <div class="col col-md-4">
                   <movie-suggest :id="index+'-suggest'" :index="index" @fill="fillOption"></movie-suggest>
                 </div>
-              </div> <!-- row -->
+              </div>
             </div>
             <div class="row">
               <div class="col">
@@ -23,41 +24,70 @@
               </div>
             </div>
           </div>
-
+          
+          <!-- Number of Minutes -->
           <div class="form-group">
             <div class="row">
               <div class="col col-md-auto">
                 <label for="numberOfMinutes">Number of Minutes</label> 
               </div>
               <div class="col col-md-2">
-                <select class="form-control" id="numberOfMinutes" v-model="minutes">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>5</option>
-                  <option>8</option>
-                </select>
+                <input class="form-control" id="numberOfMinutes" type="number" pattern="[0-9]" min="1" max="10" v-model="minutes"></input>
               </div>
             </div>
           </div>
 
+          <!-- Number of Votes -->
           <div class="form-group">
             <div class="row">
               <div class="col col-md-auto">
                 <label for="numberOfVotes">Number of Votes</label> 
               </div>
               <div class="col col-md-2">
-                <select class="form-control" id="numberOfVotes" v-model="votes">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                </select>
+                <input class="form-control" id="numberOfVotes" type="number" pattern="[0-9]" min="1" max="4" v-model="votes"></input>
               </div>
             </div>
-          </div> <!--form-group-->
+          </div>
+
+          <!-- Nominations Checkbox -->
+          <div class="form-group">
+            <div class="row">
+              <div class="col col-md-auto">
+                <label class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" v-model="haveNominations">
+                  <span class="custom-control-indicator"></span>
+                  <span class="custom-control-description">Let's have a nomination phase!</span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <!-- Nomination Length -->
+          <div class="form-group" v-if="haveNominations">
+            <div class="row">
+              <div class="col col-md-auto">
+                <label for="nominationLength">Nomination Time Limit (mins)</label>
+              </div>
+              <div class="col col-md-2">
+                <input class="form-control" id="nominationLength" type="number" pattern="[0-9]*" min="1" max="60" v-model="nominationsMinutes">
+              </div>
+            </div>
+          </div>
+
+          <!-- Number of Nomination -->
+          <div class="form-group" v-if="haveNominations">
+            <div class="row">
+              <div class="col col-md-auto">
+                <label for="numberOfNominations">Number of Nominations</label>
+              </div>
+              <div class="col col-md-2">
+                <input class="form-control" id="numberOfNominations" type="number" pattern="[0-9]" min="1" max="5" v-model="nominations">
+              </div>
+            </div>
+          </div>
 
         </form>
+
       </div>
     </div>
     <div class="row">
@@ -87,6 +117,9 @@ export default {
     return {
       minutes: '3',
       votes: '2',
+      haveNominations: false,
+      nominationsMinutes: '3',
+      nominations: '3',
       options: [{name: "", film_id: null}],
       placeholders: ['The Assassin', 'Zoolander 2', 'Titanic 2', 'Beauty and the Beast']
     }
