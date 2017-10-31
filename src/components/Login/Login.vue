d6bf604474121e08bca0b57cf18c7c5a4af1d9c8
Arran France <arranf@gmail.com>
2017-10-31 20:01:30 +0000

    Add e2e tests for the login component

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -7,22 +7,23 @@
       Oops that username & password combination wasn't quite correct.
     </div>
     <div v-if="!isInternalLogin">
-      <mdl-button raised class="btn-facebook btn-block mdl-js-ripple-effect" @click.native.prevent="facebookLogin()">Log In with <i class="fa fa-facebook-official" title="Facebook"></i><span class="sr-only">Facebook</span></mdl-button>
+      <mdl-button id="facebook" raised class="btn-facebook btn-block mdl-js-ripple-effect" @click.native.prevent="facebookLogin()">Log In with <i class="fa fa-facebook-official" title="Facebook"></i><span class="sr-only">Facebook</span></mdl-button>
       <p class="py-1 my-1">or</p>
-      <mdl-button @click.native.prevent="swapLoginType()" colored raised class="btn-block mdl-js-ripple-effect">Log In</mdl-button>
+      <mdl-button id="login" @click.native.prevent="swapLoginType()" colored raised class="btn-block mdl-js-ripple-effect">Log In</mdl-button>
       <div class="pt-1">
-        <a href="#" @click.prevent="toSignUp()" >Not got an account? Sign up</a>
+        <a id="signup" href="#" @click.prevent="toSignUp()" >Not got an account? Sign up</a>
       </div>
     </div>
+    <!-- Login Form -->
     <form class="d-flex flex-column align-items-center justify-content-center" v-if="isInternalLogin">
-      <div>
+      <div class="w-100">
         <i class="fa fa-inbox px-2 mdl-color-text--grey-600"></i> <mdl-textfield floating-label="Email" type="email" v-model="email" />
       </div>
-      <div>
+      <div class="w-100">
         <i class="fa fa-key px-2 mdl-color-text--grey-600"></i> <mdl-textfield floating-label="Password" type="password" v-model="password" />
       </div>
-      <mdl-button accent raised class="mdl-js-ripple-effect" :disabled="isDisabled" @click.native.prevent="tryLogin()">Submit</mdl-button>
-      <mdl-button class="mdl-js-ripple-effect" @click.native.prevent="swapLoginType()">Back</mdl-button>
+      <mdl-button id="submit" accent raised class="mdl-js-ripple-effect" :disabled="isDisabled" @click.native.prevent="tryLogin()">Submit</mdl-button>
+      <mdl-button id="back" class="mdl-js-ripple-effect" @click.native.prevent="swapLoginType()">Back</mdl-button>
     </form>
   </div>
 </template>
@@ -82,7 +83,9 @@ export default {
   computed: {
     ...mapState('auth', ['user']),
     isDisabled: function () {
-      return !(this.password && this.email)
+      // W3 Email regex: http://emailregex.com/
+      const regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      return !(this.password && this.email && regex.test(this.email))
     }
   },
   components: {
--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,7 +2,7 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
+  'login': function (browser) {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
@@ -11,8 +11,43 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
+      // Inital page
       .assert.containsText('h1', 'Login')
-      .assert.elementCount('.btn', 2)
+      .assert.elementCount('button', 2)
+      .assert.elementPresent('#login')
+      .assert.elementPresent('#facebook')
+      // Should be taken to login form
+      .click('#login')
+      .assert.elementCount('input', 2) // Email and password
+      .assert.elementNotPresent('#facebook')
+      .assert.elementNotPresent('#login')
+      .assert.attributeEquals('#submit', 'disabled', 'true')
+      // Test invalid form input, the submit button should still be disabled
+      .setValue('input[type=email]', 'aninvalidemail')
+      .setValue('input[type=password]', 'password')
+      .assert.attributeEquals('#submit', 'disabled', 'true')
+      // Test valid input, the submit button should not be disabled
+      .setValue('input[type=email]', 'valid@email.com')
+      // .assert.attributeEquals('#submit', 'disabled', null)
+      .assert.isNotDisabled('#submit')
+      .click('#back')
+      // We should be on the same page
+      .assert.elementPresent('#facebook')
+      .assert.elementPresent('#login')
+      // Should be taken to signup route
+      .click('#signup')
+      .assert.urlEquals(devServer + '/signup')
+      .end()
+  },
+  'facebookLogin': function (browser) {
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .click('#facebook')
+      .pause(1000)
+      .assert.urlContains('https://www.facebook.com')
       .end()
   }
 }
