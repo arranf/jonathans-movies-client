b6a068370051210127fbf3d4f25e8b580456715b
Arran France <arranf@gmail.com>
2017-11-09 14:14:51 +0000

    Make create page material

--- src/components/Create/Create.vue
+++ src/components/Create/Create.vue
@@ -1,125 +1,51 @@
 <template>
-  <div class="container d-flex flex-column align-items-center justify-content-center text-center  mb-3">
-    <div class="row">
-      <div class="col">
-        <h2 class="mt-4 pb-3">Create a Poll</h2>
-        <form autocomplete="off">
-          <input autocomplete="false" name="hidden" type="text" style="display:none;">
+  <div class="d-flex flex-column align-items-center justify-content-center text-center mb-3">
+    <h2 class="mt-4 pb-3">Create a Poll</h2>
+    <form autocomplete="off">
+      <!-- Prevent automcomplete -->
+      <input autocomplete="false" name="hidden" type="text" style="display:none;">
          
-          <!-- Options Input -->
-          <div class="form-group">
-            <div v-for="(option, index) in options" :key="index">
-              <div class="row my-2">
-                <div class="col-12">
-                  <label :for="index">Option {{ index + 1 }}</label>
-                </div>
-                <div class="col">
-                  <movie-suggest :id="index+'-suggest'" :index="index" :needed="!haveNominations" @fill="fillOption"></movie-suggest>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <button type="submit" role="button" class="btn btn-sm btn-primary" @click.prevent="addOption()"><i class="fa fa-plus" aria-disabled="true"><span class="sr-only">Add Option</span></i></button>
-              </div>
-            </div>
-          </div>
-          
-          <!-- Number of Minutes -->
-          <div class="form-group">
-            <div class="row">
-              <div class="col col-md-auto">
-                <label for="numberOfMinutes">Voting Length</label> 
-              </div>
-              <div class="col col-md-6">
-                <div class="input-group">
-                  <input name="numberOfMinutes" class="form-control" :class="{'is-invalid': errors.has('numberOfMinutes')}" data-vv-delay="1000" type="number" pattern="[0-9]" min="1" max="10" v-model="minutes" v-validate="'required|numeric|between:1,10'"></input>
-                  <span class="input-group-addon">mins</span>
-                </div>
-                <div v-show="errors.has('numberOfMinutes')" class="invalid-feedback">
-                  Please provide a valid time limit between 1 and 10 mins
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Number of Votes -->
-          <div class="form-group">
-            <div class="row">
-              <div class="col col-md-auto">
-                <label for="numberOfVotes">Number of Votes</label> 
-              </div>
-              <div class="col col-md-6">
-                <input name="numberOfVotes" class="form-control" :class="{'is-invalid': errors.has('numberOfVotes')}" data-vv-delay="1000" type="number" pattern="[0-9]" min="1" max="4" v-model="votes" v-validate="'required|numeric|between:1,4'"></input>
-                <div v-show="errors.has('numberOfVotes')" class="invalid-feedback">
-                  Please provide a valid number of votes between 1 and 4
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Nominations Checkbox -->
-          <div class="form-group">
-            <div class="row">
-              <div class="col col-md-auto">
-                <label class="custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" v-model="haveNominations">
-                  <span class="custom-control-indicator"></span>
-                  <span class="custom-control-description">Let's have a nomination phase!</span>
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <!-- Nomination Length -->
-          <div class="form-group" v-if="haveNominations">
-            <div class="row">
-              <div class="col col-md-auto">
-                <label for="nominationLength">Nomination Length</label>
-              </div>
-              <div class="col col-md-6">
-                <div class="input-group">
-                  <input name="nominationLength" class="form-control" :class="{'is-invalid': errors.has('nominationLength')}" data-vv-delay="1000" type="number" pattern="[0-9]*" min="1" max="60" v-model="nominationsMinutes" v-validate="nominationLengthRules">
-                  <span class="input-group-addon">mins</span>
-                </div>
-                <div v-show="errors.has('nominationLength')" class="invalid-feedback">
-                  Please provide a valid nomination time limit between 1 and 60 mins
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Number of Nomination -->
-          <div class="form-group" v-if="haveNominations">
-            <div class="row">
-              <div class="col col-md-auto">
-                <label for="numberOfNominations">Number of Nominations</label>
-              </div>
-              <div class="col col-md-auto">
-                <input name="numberOfNominations" class="form-control" :class="{'is-invalid': errors.has('numberOfNominations')}" data-vv-delay="1000" type="number" pattern="[0-9]" min="1" max="5" v-model="nominations" v-validate="numberOfNominationsRules">
-                <div v-show="errors.has('numberOfNominations')" class="invalid-feedback">
-                  Please provide a valid number of nominations between 1 and 4
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </form>
-
+      <!-- Options Input -->
+      <div v-for="(option, index) in options" :key="index">
+        <div class="w-100 my-2">
+          <movie-suggest :id="index+'-suggest'" :index="index" :needed="!haveNominations" @fill="fillOption"></movie-suggest>
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="col col-md-auto">
-        <button type="submit" role="button" class="btn btn-primary" @click.prevent="startPoll()" :disabled="!canStart">Start Poll!</button>
+      <div class="w-100">
+          <mdl-button colored id="addAnother" class="mdl-js-ripple-effect" @click.native.prevent="addOption()"><i class="fa fa-plus" aria-disabled="true"><span class="sr-only">Add Option</span></i></mdl-button>  
       </div>
-      <div class="col col-md-3">
-        <button type="submit" role="button" class="btn btn-outline-secondary" @click.prevent="toHome()">Back</button>
+      
+      <div class="w-100">
+        <mdl-textfield floating-label="Voting Length" type="number" pattern="[1-9]" min="1" max="10" error="Provide a number of minutes between 1 and 9" v-model="minutes" />
       </div>
+
+      <div class="w-100">
+        <mdl-textfield floating-label="Number of Votes" type="number" pattern="[1-4]" min="1" max="4" error="Provide a number of votes between 1 and 4" v-model="votes" />
+      </div>
+
+      <div class="w-100">
+        <mdl-switch id="nomination-phase" v-model="haveNominations">{{haveNominations ? 'Have Nominations' : 'Don\'t Have Nominations' }}</mdl-switch>
+      </div>
+
+      <div v-if="haveNominations">
+        <div class="w-100">
+          <mdl-textfield floating-label="Nominations Length" type="number" pattern="[1-9][0-9]*" min="1" max="60" error="Provide a number of minutes between 1 and 60" v-model="nominationsMinutes" />
+        </div>
+
+        <div class="w-100">
+          <mdl-textfield floating-label="Number of Votes" type="number" pattern="[1-4]" min="1" max="4" error="Provide a number of nominations between 1 and 4" v-model="nominations" />
+        </div>
+      </div>
+    </form>
+    <div class="w-100">
+      <mdl-button accent raised class="mdl-js-ripple-effect" @click.native.prevent="startPoll()" :disabled="!canStart">Start Poll</mdl-button>
+      <mdl-button class="mdl-js-ripple-effect" @click.native.prevent="toHome()">Back</mdl-button>
     </div>
   </div>
 </template>
 
 <script>
+import { MdlButton, MdlTextfield, MdlSwitch } from 'vue-mdl'
 import MovieSuggest from './MovieSuggest'
 import {mapActions, mapGetters, mapState} from 'vuex'
 import router from '@/router'
@@ -127,7 +53,10 @@ import router from '@/router'
 export default {
   name: 'Create',
   components: {
-    MovieSuggest
+    MovieSuggest,
+    MdlButton,
+    MdlTextfield,
+    MdlSwitch
   },
   data () {
     return {
@@ -141,9 +70,6 @@ export default {
   },
   methods: {
     ...mapActions('poll', {createPoll: 'create', updatePoll: 'update'}),
-    toHome: function () {
-      router.push('/home')
-    },
     addOption: function () {
       this.options.push({name: '', film_id: null})
     },
@@ -193,12 +119,6 @@ export default {
           this.nominationsMinutes &&
           this.nominations)
         )
-    },
-    nominationLengthRules: function () {
-      return this.haveNominations ? 'required|numeric|between:1,60' : ''
-    },
-    numberOfNominationsRules: function () {
-      return this.haveNominations ? 'required|numeric|between:1,4' : ''
     }
   }
 }
