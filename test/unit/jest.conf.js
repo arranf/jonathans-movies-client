ea938ed725d9b1b98d913571bd080d993c60ee9d
Arran France <arranf@gmail.com>
2018-01-03 10:52:44 +0000

    Disable unit coverage

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -23,8 +23,8 @@ module.exports = {
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
-  collectCoverage: true,
+  mapCoverage: false,
+  collectCoverage: false,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
