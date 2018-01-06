e30c5c3b10efd36d69028afd1c3c207b19b057c3
Arran France <arranf@gmail.com>
2018-01-06 18:49:08 +0000

    Safer check for process

--- src/api/feathers-client.js
+++ src/api/feathers-client.js
@@ -6,7 +6,7 @@ import io from 'socket.io-client'
 
 let socket
 // Change Netlify staging env variable to 1 if the staging server is setup, else develop will use production API
-if (process.env.BRANCH && process.env.BRANCH === 'develop' && process.env.STAGING === '1') {
+if (process.env && process.env.BRANCH && process.env.BRANCH === 'develop' && process.env.STAGING === '1') {
   socket = io('https://staging-api.jonathansmovies.com', {'transports': ['websocket']})
 } else if (process.env.NODE_ENV === 'production') {
   socket = io('https://api.jonathansmovies.com', {'transports': ['websocket']})
