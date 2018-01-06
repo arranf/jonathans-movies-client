696d0f2d929c9b938db066db69b1b3cebd8b69b6
Arran France <arranf@gmail.com>
2018-01-06 18:57:32 +0000

    More attempts to debug the mystical error
    Pinned it down to a socket.io issue

--- src/api/feathers-client.js
+++ src/api/feathers-client.js
@@ -7,11 +7,26 @@ import io from 'socket.io-client'
 let socket
 // Change Netlify staging env variable to 1 if the staging server is setup, else develop will use production API
 if (process.env && process.env.BRANCH && process.env.BRANCH === 'develop' && process.env.STAGING === '1') {
-  socket = io('https://staging-api.jonathansmovies.com', {'transports': ['websocket']})
+  try {
+    socket = io('https://staging-api.jonathansmovies.com', {'transports': ['websocket']})
+  } catch (e) {
+    console.error(e)
+    console.error('Unable to create socket to staging server')
+  }
 } else if (process.env.NODE_ENV === 'production') {
-  socket = io('https://api.jonathansmovies.com', {'transports': ['websocket']})
+  try {
+    socket = io('https://api.jonathansmovies.com', {'transports': ['websocket']})
+  } catch (e) {
+    console.error(e)
+    console.error('Unable to create socket to live server')
+  }
 } else {
-  socket = io('http://localhost:3030', {'transports': ['websocket']})
+  try {
+    socket = io('http://localhost:3030', {'transports': ['websocket']})
+  } catch (e) {
+    console.error(e)
+    console.error('Unable to create local development socket')
+  }
 }
 
 console.log(process.env)
