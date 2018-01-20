21b29f3a7705477500f8773d94b92e4ac03a8f24
Arran France <arranf@gmail.com>
2018-01-20 11:43:23 +0000

    Change toolbar title from 'Title' to 'Jonathan's Movies'

--- src/components/skeleton/Toolbar.vue
+++ src/components/skeleton/Toolbar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-toolbar dark color="primary">
  <v-toolbar-side-icon @click="$emit('toggleNavigation')" v-if="user"></v-toolbar-side-icon>
-    <v-toolbar-title v-if="!showMovieSearch" class="white--text">Title</v-toolbar-title>
+    <v-toolbar-title v-if="!showMovieSearch" class="white--text">Jonathan's Movies</v-toolbar-title>
     <v-spacer></v-spacer>
     <!-- <md-autocomplete v-if="showMovieSearch"
           class="search"
