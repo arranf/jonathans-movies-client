48b5cce5d0472c9bca4b70de42400e57e6db4b3b
Arran France <arranf@gmail.com>
2018-05-18 18:32:45 +0100

    Change progress indicator to red when it's running out of time

--- src/components/skeleton/ProgressBar.vue
+++ src/components/skeleton/ProgressBar.vue
@@ -1,14 +1,22 @@
 <template>
-  <v-progress-linear color="secondary" v-if="getActivePoll" v-model="percentageOfCurrentPhaseComplete" />
+  <v-progress-linear :color="color" v-if="getActivePoll" v-model="percentageOfCurrentPhaseComplete" />
 </template>
 
 <script>
 import {mapGetters} from 'vuex'
 
+const dangerMark = 80
+
 export default {
   name: 'ProgressBar',
   computed: {
-    ...mapGetters('poll', ['getActivePoll', 'percentageOfCurrentPhaseComplete'])
+    ...mapGetters('poll', ['getActivePoll', 'percentageOfCurrentPhaseComplete']),
+    color () {
+      if (this.percentageOfCurrentPhaseComplete < dangerMark) {
+        return 'secondary'
+      }
+      return 'red'
+    }
   }
 }
 </script>
