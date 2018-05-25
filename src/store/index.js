8389a38ca453b4e4194cc0f56d730f48ce8ec8a0
Arran France <arranf@gmail.com>
2018-05-25 21:40:14 +0100

    Add FeathersVuex plugin

--- src/store/index.js
+++ src/store/index.js
@@ -12,8 +12,9 @@ import pollGetters from './poll/getters'
 import time from '@/store/time'
 import snackbar from '@/store/snackbar'
 
+const { service, auth, FeathersVuex } = feathersVuex(feathersClient, { idField: '_id' })
+Vue.use(FeathersVuex)
 Vue.use(Vuex)
-const { service, auth } = feathersVuex(feathersClient, { idField: '_id' })
 
 let plugins = [
   service('vote', {getters: voteGetters}),
