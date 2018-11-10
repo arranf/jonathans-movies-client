794525433ce09d16d0d3226c53e218583cd2f126
Arran France <arranf@gmail.com>
2018-11-10 12:42:22 +0000

    Add critical CSS inlining and bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin // eslint-disable-line no-unused-vars
+const Critters = require('critters-webpack-plugin')
+
 module.exports = {
   pwa: {
     name: 'Jonathans Movies'
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new Critters()
+          // ,new BundleAnalyzerPlugin()
+        ]
+      }
+    }
   }
 }
