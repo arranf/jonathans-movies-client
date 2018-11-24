95fbf690aa0df20c08fd6bd231f385269c078fcb
Arran France <arranf@gmail.com>
2018-11-24 13:12:25 +0000

    Add quote to end of infinite scroll discover

---
+++ src/components/Discover/quotes.js
@@ -0,0 +1,602 @@
+export default [
+  {
+    'film': 'Gone with the Wind',
+    'character': 'Rhett Butler',
+    'quotation': "\"Frankly, my dear, I don't give a damn.\"",
+    'year': 1939
+  },
+  {
+    'film': 'The Godfather',
+    'character': 'Vito Corleone',
+    'quotation': "\"I'm gonna make him an offer he can't refuse.\"",
+    'year': 1972
+  },
+  {
+    'film': 'On the Waterfront',
+    'character': 'Terry Malloy',
+    'quotation': "\"You don't understand! I coulda had class. I coulda been a contender. I \ncould've been somebody, instead of a bum, which is what I am.\"",
+    'year': 1954
+  },
+  {
+    'film': 'The Wizard of Oz',
+    'character': 'Dorothy Gale',
+    'quotation': "\"Toto, I've a feeling we're not in Kansas anymore.\"",
+    'year': 1939
+  },
+  {
+    'film': 'Casablanca',
+    'character': 'Rick Blaine',
+    'quotation': "\"Here's looking at you, kid.\"",
+    'year': 1942
+  },
+  {
+    'film': 'Sudden Impact',
+    'character': 'Harry Callahan',
+    'quotation': '"Go ahead, make my day."',
+    'year': 1983
+  },
+  {
+    'film': 'Sunset Boulevard',
+    'character': 'Norma Desmond',
+    'quotation': "\"All right, Mr. DeMille, I'm ready for my close-up.\"",
+    'year': 1950
+  },
+  {
+    'film': 'Star Wars',
+    'character': 'Han Solo',
+    'quotation': '"May the Force be with you."',
+    'year': 1977
+  },
+  {
+    'film': 'All About Eve',
+    'character': 'Margo Channing',
+    'quotation': "\"Fasten your seatbelts. It's going to be a bumpy night.\"",
+    'year': 1950
+  },
+  {
+    'film': 'Taxi Driver',
+    'character': 'Travis Bickle',
+    'quotation': "\"You talkin' to me?\"",
+    'year': 1976
+  },
+  {
+    'film': 'Cool Hand Luke',
+    'character': 'Captain',
+    'quotation': "\"What we've got here is failure to communicate.\"",
+    'year': 1967
+  },
+  {
+    'film': 'Apocalypse Now',
+    'character': 'Lt. Col. Bill Kilgore',
+    'quotation': '"I love the smell of napalm in the morning."',
+    'year': 1979
+  },
+  {
+    'film': 'Love Story',
+    'character': 'Oliver Barrett IV',
+    'quotation': "\"Love means never having to say you're sorry.\"",
+    'year': 1970
+  },
+  {
+    'film': 'The Maltese Falcon',
+    'character': 'Sam Spade',
+    'quotation': '"The stuff that dreams are made of."',
+    'year': 1941
+  },
+  {
+    'film': 'E.T. the Extra-Terrestrial',
+    'character': 'E.T.',
+    'quotation': '"E.T. phone home."',
+    'year': 1982
+  },
+  {
+    'film': 'In the Heat of the Night',
+    'character': 'Virgil Tibbs',
+    'quotation': '"They call me Mister Tibbs!"',
+    'year': 1967
+  },
+  {
+    'film': 'Citizen Kane',
+    'character': 'Charles Foster Kane',
+    'quotation': '"Rosebud."',
+    'year': 1941
+  },
+  {
+    'film': 'White Heat',
+    'character': 'Arthur "Cody" Jarrett',
+    'quotation': '"Made it, Ma! Top of the world!"',
+    'year': 1949
+  },
+  {
+    'film': 'Network',
+    'character': 'Howard Beale',
+    'quotation': "\"I'm as mad as hell, and I'm not going to take this anymore!\"",
+    'year': 1976
+  },
+  {
+    'film': 'Casablanca',
+    'character': 'Rick Blaine',
+    'quotation': '"Louis, I think this is the beginning of a beautiful friendship."',
+    'year': 1942
+  },
+  {
+    'film': 'The Silence of the Lambs',
+    'character': 'Dr. Hannibal Lecter',
+    'quotation': '"A census taker once tried to test me. I ate his liver with some fava beans \nand a nice Chianti."',
+    'year': 1991
+  },
+  {
+    'film': 'Dr. No',
+    'character': 'James Bond',
+    'quotation': '"Bond. James Bond."',
+    'year': 1962
+  },
+  {
+    'film': 'The Wizard of Oz',
+    'character': 'Dorothy Gale',
+    'quotation': "\"There's no place like home.\"",
+    'year': 1939
+  },
+  {
+    'film': 'Sunset Boulevard',
+    'character': 'Norma Desmond',
+    'quotation': "\"I am big! It's the pictures that got small.\"",
+    'year': 1950
+  },
+  {
+    'film': 'Jerry Maguire',
+    'character': 'Rod Tidwell',
+    'quotation': '"Show me the money!"',
+    'year': 1996
+  },
+  {
+    'film': 'She Done Him Wrong',
+    'character': 'Lady Lou',
+    'quotation': "\"Why don't you come up sometime and see me?\"",
+    'year': 1933
+  },
+  {
+    'film': 'Midnight Cowboy',
+    'character': '"Ratso" Rizzo',
+    'quotation': "\"I'm walking here! I'm walking here!\"",
+    'year': 1969
+  },
+  {
+    'film': 'Casablanca',
+    'character': 'Ilsa Lund',
+    'quotation': "\"Play it, Sam. Play 'As Time Goes By.'\"",
+    'year': 1942
+  },
+  {
+    'film': 'A Few Good Men',
+    'character': 'Col. Nathan R. Jessup',
+    'quotation': "\"You can't handle the truth!\"",
+    'year': 1992
+  },
+  {
+    'film': 'Grand Hotel',
+    'character': 'Grusinskaya',
+    'quotation': '"I want to be alone."',
+    'year': 1932
+  },
+  {
+    'film': 'Gone with the Wind',
+    'character': "Scarlett O'Hara",
+    'quotation': '"After all, tomorrow is another day!"',
+    'year': 1939
+  },
+  {
+    'film': 'Casablanca',
+    'character': 'Capt. Louis Renault',
+    'quotation': '"Round up the usual suspects."',
+    'year': 1942
+  },
+  {
+    'film': 'When Harry Met Sally...',
+    'character': 'Customer',
+    'quotation': "\"I'll have what she's having.\"",
+    'year': 1989
+  },
+  {
+    'film': 'To Have and Have Not',
+    'character': 'Marie "Slim" Browning',
+    'quotation': "\"You know how to whistle, don't you, Steve? You just put your lips together \nand blow.\"",
+    'year': 1944
+  },
+  {
+    'film': 'Jaws',
+    'character': 'Martin Brody',
+    'quotation': "\"You're gonna need a bigger boat.\"",
+    'year': 1975
+  },
+  {
+    'film': 'The Treasure of the Sierra Madre',
+    'character': '"Gold Hat"',
+    'quotation': "\"Badges? We ain't got no badges! We don't need no badges! I don't have to \nshow you any stinking badges!\"",
+    'year': 1948
+  },
+  {
+    'film': 'The Terminator',
+    'character': 'The Terminator',
+    'quotation': "\"I'll be back.\"",
+    'year': 1984
+  },
+  {
+    'film': 'The Pride of the Yankees',
+    'character': 'Lou Gehrig',
+    'quotation': '"Today, I consider myself the luckiest man on the face of the Earth."',
+    'year': 1942
+  },
+  {
+    'film': 'Field of Dreams',
+    'character': 'Shoeless Joe Jackson',
+    'quotation': '"If you build it, he will come."',
+    'year': 1989
+  },
+  {
+    'film': 'Forrest Gump',
+    'character': 'Forrest Gump',
+    'quotation': "\"Mama always said life was like a box of chocolates. You never know what \nyou're gonna get.\"",
+    'year': 1994
+  },
+  {
+    'film': 'Bonnie and Clyde',
+    'character': 'Clyde Barrow',
+    'quotation': '"We rob banks."',
+    'year': 1967
+  },
+  {
+    'film': 'The Graduate',
+    'character': 'Mr. Maguire',
+    'quotation': '"Plastics."',
+    'year': 1967
+  },
+  {
+    'film': 'Casablanca',
+    'character': 'Rick Blaine',
+    'quotation': "\"We'll always have Paris.\"",
+    'year': 1942
+  },
+  {
+    'film': 'The Sixth Sense',
+    'character': 'Cole Sear',
+    'quotation': '"I see dead people."',
+    'year': 1999
+  },
+  {
+    'film': 'A Streetcar Named Desire',
+    'character': 'Stanley Kowalski',
+    'quotation': '"Stella! Hey, Stella!"',
+    'year': 1951
+  },
+  {
+    'film': 'Now, Voyager',
+    'character': 'Charlotte Vale',
+    'quotation': "\"Oh, Jerry, don't let's ask for the moon. We have the stars.\"",
+    'year': 1942
+  },
+  {
+    'film': 'Shane',
+    'character': 'Joey Starrett',
+    'quotation': '"Shane. Shane. Come back!"',
+    'year': 1953
+  },
+  {
+    'film': 'Some Like It Hot',
+    'character': 'Osgood Fielding III',
+    'quotation': "\"Well, nobody's perfect.\"",
+    'year': 1959
+  },
+  {
+    'film': 'Frankenstein',
+    'character': 'Henry Frankenstein',
+    'quotation': "\"It's alive! It's alive!\"",
+    'year': 1931
+  },
+  {
+    'film': 'Apollo 13',
+    'character': 'Jim Lovell',
+    'quotation': '"Houston, we have a problem."',
+    'year': 1995
+  },
+  {
+    'film': 'Dirty Harry',
+    'character': 'Harry Callahan',
+    'quotation': "\"You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, \npunk?\"",
+    'year': 1971
+  },
+  {
+    'film': 'Jerry Maguire',
+    'character': 'Dorothy Boyd',
+    'quotation': "\"You had me at 'hello.'\"",
+    'year': 1996
+  },
+  {
+    'film': 'Animal Crackers',
+    'character': 'Capt. Geoffrey T. Spaulding',
+    'quotation': "\"One morning I shot an elephant in my pajamas. How he got in my pajamas, I \ndon't know.\"",
+    'year': 1930
+  },
+  {
+    'film': 'A League of Their Own',
+    'character': 'Jimmy Dugan',
+    'quotation': "\"There's no crying in baseball!\"",
+    'year': 1992
+  },
+  {
+    'film': 'Annie Hall',
+    'character': 'Annie Hall',
+    'quotation': '"La-dee-da, la-dee-da."',
+    'year': 1977
+  },
+  {
+    'film': 'Psycho',
+    'character': 'Norman Bates',
+    'quotation': "\"A boy's best friend is his mother.\"",
+    'year': 1960
+  },
+  {
+    'film': 'Wall Street',
+    'character': 'Gordon Gekko',
+    'quotation': '"Greed, for lack of a better word, is good."',
+    'year': 1987
+  },
+  {
+    'film': 'The Godfather Part II',
+    'character': 'Michael Corleone',
+    'quotation': '"Keep your friends close, but your enemies closer."',
+    'year': 1974
+  },
+  {
+    'film': 'Gone with the Wind',
+    'character': "Scarlett O'Hara",
+    'quotation': "\"As God is my witness, I'll never be hungry again.\"",
+    'year': 1939
+  },
+  {
+    'film': 'Sons of the Desert',
+    'character': 'Oliver',
+    'quotation': "\"Well, here's another nice mess you've gotten me into!\"",
+    'year': 1933
+  },
+  {
+    'film': 'Scarface',
+    'character': 'Tony Montana',
+    'quotation': "\"Say 'hello' to my little friend!\"",
+    'year': 1983
+  },
+  {
+    'film': 'Beyond the Forest',
+    'character': 'Rosa Moline',
+    'quotation': '"What a dump."',
+    'year': 1949
+  },
+  {
+    'film': 'The Graduate',
+    'character': 'Benjamin Braddock',
+    'quotation': "\"Mrs. Robinson, you're trying to seduce me. Aren't you?\"",
+    'year': 1967
+  },
+  {
+    'film': 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+    'character': 'President Merkin Muffley',
+    'quotation': "\"Gentlemen, you can't fight in here! This is the War Room!\"",
+    'year': 1964
+  },
+  {
+    'film': 'The Adventures of Sherlock Holmes',
+    'character': 'Sherlock Holmes',
+    'quotation': '"Elementary, my dear Watson."',
+    'year': 1939
+  },
+  {
+    'film': 'Planet of the Apes',
+    'character': 'George Taylor',
+    'quotation': '"Get your stinking paws off me, you damned dirty ape."',
+    'year': 1968
+  },
+  {
+    'film': 'Casablanca',
+    'character': 'Rick Blaine',
+    'quotation': '"Of all the gin joints in all the towns in all the world, she walks into \nmine."',
+    'year': 1942
+  },
+  {
+    'film': 'The Shining',
+    'character': 'Jack Torrance',
+    'quotation': "\"Here's Johnny!\"",
+    'year': 1980
+  },
+  {
+    'film': 'Poltergeist',
+    'character': 'Carol Anne Freeling',
+    'quotation': "\"They're here!\"",
+    'year': 1982
+  },
+  {
+    'film': 'Marathon Man',
+    'character': 'Dr. Christian Szell',
+    'quotation': '"Is it safe?"',
+    'year': 1976
+  },
+  {
+    'film': 'The Jazz Singer',
+    'character': 'Jakie Rabinowitz/Jack Robin',
+    'quotation': "\"Wait a minute, wait a minute. You ain't heard nothin' yet!\"",
+    'year': 1927
+  },
+  {
+    'film': 'Mommie Dearest',
+    'character': 'Joan Crawford',
+    'quotation': '"No wire hangers, ever!"',
+    'year': 1981
+  },
+  {
+    'film': 'Little Caesar',
+    'character': 'Cesare Enrico "Rico" Bandello',
+    'quotation': '"Mother of mercy, is this the end of Rico?"',
+    'year': 1931
+  },
+  {
+    'film': 'Chinatown',
+    'character': 'Lawrence Walsh',
+    'quotation': "\"Forget it, Jake, it's Chinatown.\"",
+    'year': 1974
+  },
+  {
+    'film': 'A Streetcar Named Desire',
+    'character': 'Blanche DuBois',
+    'quotation': '"I have always depended on the kindness of strangers."',
+    'year': 1951
+  },
+  {
+    'film': 'Terminator 2: Judgment Day',
+    'character': 'The Terminator',
+    'quotation': '"Hasta la vista, baby."',
+    'year': 1991
+  },
+  {
+    'film': 'Soylent Green',
+    'character': 'Det. Robert Thorn',
+    'quotation': '"Soylent Green is people!"',
+    'year': 1973
+  },
+  {
+    'film': '2001: A Space Odyssey',
+    'character': 'Dave Bowman',
+    'quotation': '"Open the pod bay doors, HAL."',
+    'year': 1968
+  },
+  {
+    'film': 'Airplane!',
+    'character': 'Ted Striker and Dr. Rumack',
+    'quotation': "Striker: \"Surely you can't be serious.\"\nRumack: \"I am serious … and don't call me Shirley.\"",
+    'year': 1980
+  },
+  {
+    'film': 'Rocky',
+    'character': 'Rocky Balboa',
+    'quotation': '"Yo, Adrian!"',
+    'year': 1976
+  },
+  {
+    'film': 'Funny Girl',
+    'character': 'Fanny Brice',
+    'quotation': '"Hello, gorgeous."',
+    'year': 1968
+  },
+  {
+    'film': "National Lampoon's Animal House",
+    'character': 'John "Bluto" Blutarsky',
+    'quotation': '"Toga! Toga!"',
+    'year': 1978
+  },
+  {
+    'film': 'Dracula',
+    'character': 'Count Dracula',
+    'quotation': '"Listen to them. Children of the night. What music they make."',
+    'year': 1931
+  },
+  {
+    'film': 'King Kong',
+    'character': 'Carl Denham',
+    'quotation': "\"Oh, no, it wasn't the airplanes. It was Beauty killed the Beast.\"",
+    'year': 1933
+  },
+  {
+    'film': 'The Lord of the Rings: The Two Towers',
+    'character': 'Gollum',
+    'quotation': '"My precious."',
+    'year': 2002
+  },
+  {
+    'film': 'Dog Day Afternoon',
+    'character': 'Sonny Wortzik',
+    'quotation': '"Attica! Attica!"',
+    'year': 1975
+  },
+  {
+    'film': '42nd Street',
+    'character': 'Julian Marsh',
+    'quotation': "\"Sawyer, you're going out a youngster, but you've got to come back a star!\"",
+    'year': 1933
+  },
+  {
+    'film': 'On Golden Pond',
+    'character': 'Ethel Thayer',
+    'quotation': "\"Listen to me, mister. You're my knight in shining armor. Don't you forget \nit. You're going to get back on that horse, and I'm going to be right \nbehind you, holding on tight, and away we're gonna go, go, go!\"",
+    'year': 1981
+  },
+  {
+    'film': 'Knute Rockne, All American',
+    'character': 'Knute Rockne',
+    'quotation': "\"Tell 'em to go out there with all they got and win just one for the Gipper\n.\"",
+    'year': 1940
+  },
+  {
+    'film': 'Goldfinger',
+    'character': 'James Bond',
+    'quotation': '"A martini. Shaken, not stirred."',
+    'year': 1964
+  },
+  {
+    'film': 'The Naughty Nineties',
+    'character': 'Dexter',
+    'quotation': "\"Who's on first?\"",
+    'year': 1945
+  },
+  {
+    'film': 'Caddyshack',
+    'character': 'Carl Spackler',
+    'quotation': "\"Cinderella story. Outta nowhere. A former greenskeeper, now, about to \nbecome the Masters champion. It looks like a mirac...It's in the hole! It's \nin the hole! It's in the hole!\"",
+    'year': 1980
+  },
+  {
+    'film': 'Auntie Mame',
+    'character': 'Mame Dennis',
+    'quotation': '"Life is a banquet, and most poor suckers are starving to death!"',
+    'year': 1958
+  },
+  {
+    'film': 'Top Gun',
+    'character': 'Lt. Pete "Maverick" Mitchell and Lt.jg Nick "Goose" Bradshaw',
+    'quotation': '"I feel the need—the need for speed!"',
+    'year': 1986
+  },
+  {
+    'film': 'Dead Poets Society',
+    'character': 'John Keating',
+    'quotation': '"Carpe diem. Seize the day, boys. Make your lives extraordinary."',
+    'year': 1989
+  },
+  {
+    'film': 'Moonstruck',
+    'character': 'Loretta Castorini',
+    'quotation': '"Snap out of it!"',
+    'year': 1987
+  },
+  {
+    'film': 'Yankee Doodle Dandy',
+    'character': 'George M. Cohan',
+    'quotation': '"My mother thanks you. My father thanks you. My sister thanks you. And I \nthank you."',
+    'year': 1942
+  },
+  {
+    'film': 'Dirty Dancing',
+    'character': 'Johnny Castle',
+    'quotation': '"Nobody puts Baby in a corner."',
+    'year': 1987
+  },
+  {
+    'film': 'The Wizard of Oz',
+    'character': 'Wicked Witch of the West',
+    'quotation': "\"I'll get you, my pretty, and your little dog too!\"",
+    'year': 1939
+  },
+  {
+    'film': 'Titanic',
+    'character': 'Jack Dawson',
+    'quotation': "\"I'm the King of the World!\" ",
+    'year': 1997
+  }
+]
