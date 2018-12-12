98ec9ea34d3474a5fe3bdbfadc39568da0344a45
Arran France <arranf@gmail.com>
2018-12-12 11:56:02 +0000

    Add house to vuex store

---
+++ src/store/house.js
@@ -0,0 +1,36 @@
+import feathersClient from '@/api/feathers-client'
+
+const state = {
+  current: ''
+}
+
+const actions = {
+  getCurrent ({ commit, state }) {
+    feathersClient.service('/house').find()
+      .then((data) => {
+        commit('setCurrent', data)
+      })
+  },
+  setCurrent ({ commit, state }, payload) {
+    commit('setCurrent', payload)
+  },
+  changeCurrent ({ commit, state }, payload) {
+    feathersClient.service('/house').patch()
+      .then((data) => {
+        commit('setCurrent', data)
+      })
+  }
+}
+
+const mutations = {
+  setCurrent (state, payload) {
+    state.current = typeof payload === 'string' ? payload : payload.current
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+}
