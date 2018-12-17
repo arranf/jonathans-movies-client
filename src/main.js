70eff3329974966cf3e49fbb3839d6733205f8f7
Arran France <arranf@gmail.com>
2018-12-17 22:56:15 +0000

    Switch to async load sentry

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from '@/components/skeleton/AppContainer'
 import router from './router'
 import store from './store/'
 import '@/api/feathers-client'
-import * as Sentry from '@sentry/browser'
 
 import './plugins/vuetify'
 import './registerServiceWorker'
@@ -15,13 +14,6 @@ const isProduction = process.env.NODE_ENV === 'production'
 Vue.config.productionTip = !isProduction
 Vue.config.devtools = !isProduction
 Vue.config.performance = !isProduction
-
-if (isProduction) {
-  Sentry.init({
-    dsn: 'https://af031047bfd341f8b9d082c5474c016d@sentry.io/1329415',
-    integrations: [new Sentry.Integrations.Vue({ Vue })]
-  })
-}
 
 new Vue({
   router,
