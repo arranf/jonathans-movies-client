1264e0b2dc1e5987986d9e8e3942623c9b9874ff
Arran France <arran@arranfrance.com>
2019-10-06 01:07:22 +0100

    Improve discover to use a queue based system preventing overlaps in requests allowing movies to appear multiple times

--- src/components/Discover/Discover.vue
+++ src/components/Discover/Discover.vue
@@ -1,62 +1,90 @@
 <template>
   <div>
-    <movie-info-modal close-route="/discover" :show.sync="showingFilm" :filmId="filmId" :show-nominate="true" />
+    <movie-info-modal
+      close-route="/discover"
+      :show.sync="showingFilm"
+      :filmId="filmId"
+      :show-nominate="true"
+    />
     <transition name="slide">
-      <v-btn v-if="seenIds.length > 24"
-            fixed
-            fab
-            bottom
-            right
-            color="secondary"
-            @click="scroll"
-            class="big-bottom"
-          >
+      <v-btn
+        v-if="seenIds.length > 24"
+        fixed
+        fab
+        bottom
+        right
+        color="secondary"
+        @click="scroll"
+        class="big-bottom"
+      >
         <v-icon>expand_less</v-icon>
       </v-btn>
     </transition>
 
-        <div v-if="recommendations && recommendations.length">
-            <h3 class="separator">Recommended For You</h3>
-            <v-container fluid grid-list-xs>
-            <v-layout row wrap>
-              <v-flex xs6 sm4 md3 lg2 :key="recommendation.film._id+index+'recommended'" v-for="(recommendation, index) in recommendations">
-                <film-preview :film="recommendation.film" :reasons="recommendation.reasons" modal-page-name="Discover"></film-preview>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </div>
-        <div v-if="recentlyAdded && recentlyAdded.length">
-          <h3 class="separator">Recently Added</h3>
-              <v-container fluid grid-list-xs>
-              <v-layout row wrap>
-                <v-flex xs6 sm4 md3 lg2 :key="recentlyAdded._id+index+'recent'" v-for="(recentlyAdded, index) in recentlyAdded">
-                  <film-preview :film="recentlyAdded" modal-page-name="Discover"></film-preview>
-                </v-flex>
-              </v-layout>
-            </v-container>
-        </div>
-        <div v-if="suggestions && suggestions.length">
-          <h3 class="separator mt-4">Popular and Highly Rated Movies</h3>
-          <v-container grid-list-md text-xs-center>
-            <v-layout row wrap>
-              <v-flex xs6 md3 lg2 :key="film._id+index" v-for="(film, index) in suggestions">
-                <film-preview :film="film" modal-page-name="Discover"></film-preview>
-              </v-flex>
-              <quote v-if="done" />
-              <v-flex xs12>
-                <v-progress-linear color="secondary" v-if="busy" indeterminate />
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </div>
+    <div v-if="recommendations && recommendations.length">
+      <h3 class="separator">Recommended For You</h3>
+      <v-container fluid grid-list-xs>
+        <v-layout row wrap>
+          <v-flex
+            xs6
+            sm4
+            md3
+            lg2
+            :key="recommendation.film._id+index+'recommended'"
+            v-for="(recommendation, index) in recommendations"
+          >
+            <film-preview
+              :film="recommendation.film"
+              :reasons="recommendation.reasons"
+              modal-page-name="Discover"
+            ></film-preview>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </div>
+    <div v-if="recentlyAdded && recentlyAdded.length">
+      <h3 class="separator">Recently Added</h3>
+      <v-container fluid grid-list-xs>
+        <v-layout row wrap>
+          <v-flex
+            xs6
+            sm4
+            md3
+            lg2
+            :key="recentlyAdded._id+index+'recent'"
+            v-for="(recentlyAdded, index) in recentlyAdded"
+          >
+            <film-preview :film="recentlyAdded" modal-page-name="Discover"></film-preview>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
+    <div v-if="suggestions && suggestions.length">
+      <h3 class="separator mt-4">Popular and Highly Rated Movies</h3>
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex xs6 md3 lg2 :key="film._id+index" v-for="(film, index) in suggestions">
+            <film-preview :film="film" modal-page-name="Discover"></film-preview>
+          </v-flex>
+          <quote v-if="done" />
+          <v-flex xs12>
+            <v-progress-linear color="secondary" v-if="busy" indeterminate />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </div>
+  </div>
 </template>
 
 <script>
 import FilmPreview from '@/components/common/FilmPreview'
 import MovieInfoModal from '@/components/common/MovieInfoModal'
 import Quote from './Quote'
-import { getRecommendations as fetchRecommendations, getRecentlyAdded as fetchRecentlyAdded, discoverMovies } from '@/api'
+import {
+  getRecommendations as fetchRecommendations,
+  getRecentlyAdded as fetchRecentlyAdded,
+  discoverMovies
+} from '@/api'
 
 import scrollListener from '@/scroll-listener'
 import { mapActions } from 'vuex'
@@ -71,7 +99,8 @@ export default {
       recentlyAdded: [],
       busy: false,
       seenIds: [],
-      done: false
+      done: false,
+      queueCount: 0
     }
   },
   components: {
@@ -90,29 +119,31 @@ export default {
   methods: {
     ...mapActions('snackbar', { setSnackbar: 'setText' }),
     ...mapActions('loading', ['setLoaded']),
-    refresh: function () {
+    refresh: async function () {
       if (this.done) {
         return
       }
-      this.busy = true
-      return discoverMovies(this.seenIds)
-        .then(discoveredFilms => {
-          // Check if we've exhausted the collection
-          if (discoveredFilms.length === 0) {
-            this.done = true
-          }
 
-          this.suggestions = this.suggestions.concat(discoveredFilms)
-          for (let i = 0; i < discoveredFilms.length; i++) {
-            this.seenIds.push(discoveredFilms[i]._id)
-          }
-          this.busy = false
-        })
-        .catch(e => {
-          console.error(e)
-          this.setSnackbar('Something went wrong. Try again.')
-          this.busy = false
-        })
+      this.busy = true
+      try {
+        const discoveredFilms = await discoverMovies(this.seenIds)
+        if (discoveredFilms.length === 0) {
+          this.done = true
+        }
+        this.suggestions = this.suggestions.concat(discoveredFilms)
+        this.seenIds = this.seenIds.concat(discoveredFilms.map(f => f._id))
+      } catch (e) {
+        console.error(e)
+        this.setSnackbar('Something went wrong. Try again.')
+        this.busy = false
+      } finally {
+        this.busy = false
+
+        if (this.queueCount > 0) {
+          this.queueCount--
+          this.refresh()
+        }
+      }
     },
     getRecommendations: function () {
       this.busy = true
@@ -142,6 +173,16 @@ export default {
     },
     scroll () {
       window.scrollTo(0, 0)
+    },
+    refreshQueueWrapper () {
+      // This queue abstraction prevents a race condition where seenIds are not sent to the server before the next request
+      // In this race duplicates may be shown extremely close together causing a weird expereince for users
+      // By queueing requests on scroll we prevent a case where requests overlap
+      if (this.busy) {
+        this.queueCount++
+      } else {
+        this.refresh()
+      }
     }
   },
   computed: {
@@ -153,19 +194,25 @@ export default {
     document.removeEventListener('scroll', this.listener)
   },
   async created () {
-    const nextPage = this.refresh
+    const nextPage = this.refreshQueueWrapper
     this.listener = scrollListener(nextPage)
     // route watcher won't be called on initial load
     this.showingFilm = Boolean(this.filmId)
     try {
-      await this.getRecommendations()
-      await this.getRecentlyAdded()
-      await this.refresh()
+      await Promise.all([
+        this.getRecommendations(),
+        this.getRecentlyAdded(),
+        this.refresh()
+      ])
       document.addEventListener('scroll', this.listener)
       await this.setLoaded('Discover')
     } catch (e) {
-      document.removeEventListener('scroll', this.listener)
+      console.log('Failed')
       console.error(e)
+      this.setSnackbar('Something went wrong. Try again.')
+      console.log(this.$router)
+      this.$router.push('/home')
+      document.removeEventListener('scroll', this.listener)
     }
   }
 }
