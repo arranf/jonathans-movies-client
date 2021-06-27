f76d665affcdab4a9251f813024ed77731f5b9d1
Arran France <arran@arranfrance.com>
2021-06-27 22:42:41 +0100

    fix: access_token exception

--- src/router/index.js
+++ src/router/index.js
@@ -171,19 +171,6 @@ const router = new Router({
   mode: "history",
   linkActiveClass: "active",
   routes,
-  scrollBehavior(to, _from, savedPosition) {
-    if (to.hash) {
-      return {
-        selector: to.hash,
-        behavior: "smooth",
-      };
-    }
-    if (savedPosition) {
-      return savedPosition;
-    } else {
-      return { x: 0, y: 0 };
-    }
-  },
 });
 
 function initStore() {
